5e3c67cb0814dee2066feff75bad67b771dc6598
yanshu <18155795465@163.com>
2024-10-30 19:47:48 +0800

    init

---
+++ src/main/java/com/wy/CWeaApplication.java
@@ -0,0 +1,14 @@
+package com.wy;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class CWeaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CWeaApplication.class, args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  启动成功   ლ(´ڡ`ლ)ﾞ      ");
+    }
+}
